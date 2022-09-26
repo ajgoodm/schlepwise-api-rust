2628ce6d00499199847a1818fd5996810a56d6ae
Aaron Goodman <aarongoodm@gmail.com>
2022-09-26 12:09:48 -0400

    diesel migrations

---
+++ migrations/2022-09-26-160118_create_chores/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE chores (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  description VARCHAR,
+  expected_duration_minutes NUMERIC,
+  household_id SERIAL NOT NULL,
+  created_by_family_member_id SERIAL,
+  CONSTRAINT fk_chores_household
+    FOREIGN KEY(household_id)
+    REFERENCES households(id),
+  CONSTRAINT fk_chores_created_by_family_member
+    FOREIGN KEY(created_by_family_member_id)
+    REFERENCES family_members(id)
+);
