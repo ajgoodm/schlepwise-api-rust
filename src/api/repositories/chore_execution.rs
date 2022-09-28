a3b47890b8f9a1626b1f92e15227d7fa2028496d
Aaron Goodman <aarongoodm@gmail.com>
2022-09-27 22:21:47 -0400

    get methods

--- src/api/repositories/chore_execution.rs
+++ src/api/repositories/chore_execution.rs
@@ -5,10 +5,8 @@ use diesel::prelude::*;
 
 use crate::api::model::{ChoreExecution, FamilyMember, Household};
 
-use crate::schema::chore_executions;
-use crate::schema::chore_executions::dsl::*;
-use crate::schema::family_members;
-use crate::schema::family_members::dsl::*;
+use crate::schema::chore_executions::dsl::{chore_executions, executed_by_family_member_id};
+use crate::schema::family_members::dsl::{family_members, household_id};
 use crate::schema::households;
 
 pub fn show_chore_executions(
@@ -17,11 +15,11 @@ pub fn show_chore_executions(
     executed_by_family_member_id_: Option<i32>,
 ) -> QueryResult<Vec<ChoreExecution>> {
     if let Some(family_member_id_) = executed_by_family_member_id_ {
-        family_members::table
+        family_members
             .find(family_member_id_)
             .filter(household_id.eq(&household_id_))
             .get_result::<FamilyMember>(connection)?;
-        chore_executions::table
+        chore_executions
             .filter(executed_by_family_member_id.eq(&family_member_id_))
             .limit(5)
             .load::<ChoreExecution>(connection)
@@ -30,11 +28,8 @@ pub fn show_chore_executions(
             .find(household_id_)
             .get_result::<Household>(connection)?;
         chore_executions
-            // .inner_join(
-            //     family_members::table
-            //         .on(family_members::id.eq(chore_executions::executed_by_family_member_id)),
-            // )
-            // .filter(family_members::household_id.eq(household_id_))
+            // .inner_join(family_members)
+            // .filter(household_id.eq(household_id_))
             .order_by(executed_by_family_member_id.asc())
             .limit(5)
             .load::<ChoreExecution>(connection)
